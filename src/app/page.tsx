19d66134bf121acdef27c3438dcfd645145b9c41
cheaperchoiceautos-oss <cheaperchoiceautos@gmail.com>
2025-09-17 16:22:07 -0500

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Cheaper Choice Rentals</h1>
+      <p>Welcome to your unified rental + sales + CRM platform.</p>
+    </div>
+  );
+}
